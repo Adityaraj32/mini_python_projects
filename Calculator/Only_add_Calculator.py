fd882d490cf40f389fda9e505853ba027ce77f4e
Aditya raj <87028110+Adityaraj32@users.noreply.github.com>
2026-04-13 00:25:32 +0530

    Create Only_add_Calculator.py

---
+++ Calculator/Only_add_Calculator.py
@@ -0,0 +1,14 @@
+# 1st method for adding two number
+
+print("Enter an number:")
+inputnum = input()
+print("Enter an number:")
+inputnum2 = input()
+
+print("The sum is ", int(inputnum) + int(inputnum2))
+
+# 2nd method for adding two number
+
+a = int(input("Enter an number: "))
+b = int(input("Enter an number: "))
+print("The sum is",a + b)
